b794e2d62c32772697c4b87f98d6740e30109830
Gregory Frye <Swayze@Gregorys-MacBook-Pro.local>
2018-09-18 17:28:01 -0500

    added homework

---
+++ js/app.js
@@ -0,0 +1,220 @@
+//1. A light switch that can be either on or off.
+
+// const switch = ()=>{
+// // store a light switch as an object and then create a function to turn it on and off
+// }
+
+// 2. A user's email address.
+
+// a string 
+// const email = 'gfrye414@yahoo.com'
+
+// 3.A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+
+// an array
+// const outerSpace = ['spaceship', 'hull', 'laser blasters', 'tractor beam', 'warp drive']
+
+// 4.A list of student names from our class.
+
+// const newArray = ['Chris','Chris','Fillip']
+// store a list of names in an array
+
+// 5.A list of student names from our class, each with a location.
+
+  // const system = { store names with location as an object
+  // student1: { 
+  //       name: 'Jon',
+  //      	location: 'desk'
+  //      },
+  // student2: {
+  // 		name: 'Charolotte'
+  // 		location: 'desk'
+  // }:
+
+// 6.A list of student names from our class, each with a location and each with a list of favorite tv shows.
+
+// const students = {
+// 	{ name: 'Greg', location: 'Home', movies: ['Roadhouse'] }
+// store the list of movies in an array and that array along with name and location as an object
+
+
+
+
+// Take it Easy
+// Make an array that holds all of the colors of the rainbow.
+// Write code that will access "blue" from the rainbow array.
+// Make an object that is called your name and holds the information about your favorite 
+// food, a hobby, the name of the town that you live in currently, and your favorite datatype.
+// Write code that will access your hobby from the object that you just created.
+
+
+
+
+// const rainbow = ['yellow', 'green', 'blue', 'red', 'orange', 'indigo', 'Violet'];
+// console.log(rainbow[2]);
+
+// const pickle = [
+
+// 	{name: 'Greg', hobby: ['Cold HotDog'], City: 'Chicago', datatype:'array'}
+// ];
+// console.log(pickle[0].hobby[0]);
+
+// Crazy Object
+
+// const crazyObject = {
+//   taco: [
+//     {
+//       meat: 'steak',
+//       cheese: ['panela', 'queso', 'chihuahua']
+//     },
+//     {
+//       meat: 'chicken',
+//       salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
+//     },
+//   ],
+//   larry: {
+//     nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+//     quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+//     characters: [
+//       {
+//         name: "Jeff",
+//         occupation: "manager"
+//       },
+//       {
+//         name: "funkhauser",
+//         occupation: "tv dude"
+//       },
+//       {
+//         name: "susie",
+//         occupation: "jeffs wife",
+//         favourtieHobby: "Swearing at Larry and Jeff"
+//       },
+//     ]
+//   }
+// }
+// // Use crazyObject to log the following.
+
+// // "omg my mouth is burning"
+// console.log(crazyObject.taco[1].salsa[5]);
+// // "Pretty pretty prettayyyyy good"
+// console.log(crazyObject.larry.quotes[0]);
+// // "Swearing at Larry and Jeff"
+// console.log(crazyObject.larry.characters[2].favourtieHobby);
+// // "Chicken Teriyaki Boyyyyyy"
+// console.log(crazyObject.larry.nicknames[1]);
+// // The object the contains the name funkhauser
+// console.log(crazyObject.larry.characters[1].name);
+
+
+
+
+
+// const inception = {
+//    reality: {
+//        dreamLayer1: {
+//            dreamLayer2: {
+//                dreamLayer3: {
+//                    dreamLayer4: {
+//                        dreamLayer5: {
+//                            dreamLayer6: {
+//                                limbo: "Joseph Gordon Levitt"
+//                           }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+// }
+
+// let limbo = null
+// console.log(inception);
+// // Change the value of limbo to null.
+
+
+
+
+
+const bondFilms = [
+  { "title" : "Skyfall", "year" : 2012, "actor" : "Daniel Craig", "gross" : "$1,108,561,008" },
+  { "title" : "Thunderball", "year" : 1965, "actor" : "Sean Connery", "gross" : "$1,014,941,117" },
+  { "title" : "Goldfinger", "year" : 1964, "actor" : "Sean Connery", "gross" : "$912,257,512" },
+  { "title" : "Live and Let Die", "year" : 1973, "actor" : "Roger Moore", "gross" : "$825,110,761" },
+  { "title" : "You Only Live Twice", "year" : 1967, "actor" : "Sean Connery", "gross" : "$756,544,419" },
+  { "title" : "The Spy Who Loved Me", "year" : 1977, "actor" : "Roger Moore", "gross" : "$692,713,752" },
+  { "title" : "Casino Royale", "year" : 2006, "actor" : "Daniel Craig", "gross" : "$669,789,482" },
+  { "title" : "Moonraker", "year" : 1979, "actor" : "Roger Moore", "gross" : "$655,872,400" },
+  { "title" : "Diamonds Are Forever", "year" : 1971, "actor" : "Sean Connery", "gross" : "$648,514,469" },
+  { "title" : "Quantum of Solace", "year" : 2008, "actor" : "Daniel Craig", "gross" : "$622,246,378" },
+  { "title" : "From Russia with Love", "year" : 1963, "actor" : "Sean Connery", "gross" : "$576,277,964" },
+  { "title" : "Die Another Day", "year" : 2002, "actor" : "Pierce Brosnan", "gross" : "$543,639,638" },
+  { "title" : "Goldeneye", "year" : 1995, "actor" : "Pierce Brosnan", "gross" : "$529,548,711" },
+  { "title" : "On Her Majesty's Secret Service", "year" : 1969, "actor" : "George Lazenby", "gross" : "$505,899,782" },
+  { "title" : "The World is Not Enough", "year" : 1999, "actor" : "Pierce Brosnan", "gross" : "$491,617,153" },
+  { "title" : "For Your Eyes Only", "year" : 1981, "actor" : "Roger Moore", "gross" : "$486,468,881" },
+  { "title" : "Tomorrow Never Dies", "year" : 1997, "actor" : "Pierce Brosnan", "gross" : "$478,946,402" },
+  { "title" : "The Man with the Golden Gun", "year" : 1974, "actor" : "Roger Moore", "gross" : "$448,249,281" },
+  { "title" : "Dr. No", "year" : 1962, "actor" : "Sean Connery", "gross" : "$440,759,072" },
+  { "title" : "Octopussy", "year" : 1983, "actor" : "Roger Moore", "gross" : "$426,244,352" },
+  { "title" : "The Living Daylights", "year" : 1987, "actor" : "Timothy Dalton", "gross" : "$381,088,866" },
+  { "title" : "A View to a Kill", "year" : 1985, "actor" : "Roger Moore", "gross" : "$321,172,633" },
+  { "title" : "License to Kill", "year" : 1989, "actor" : "Timothy Dalton", "gross" : "$285,157,191" }
+];
+
+
+// Create a new array called bondTitles with only the titles of the Bond films, and console.log 
+// the new array.
+
+const bondTitles = {};
+for(let i = 0; i < bondFilms.length; i++){
+
+console.log(bondFilms[i].title);	
+}
+// bondTitles[bondFilms[i].title] = 0
+// let bondTitles = Array[bondFilms]; 
+
+// for(let key in bondFilms)
+// 	console.log(key);
+// };
+// const bondTitles = {};
+// for(let prop in bondTitles){
+// 	console.log(prop, 'key');
+// 	console.log(bondTitles[prop], 'value');
+// }
+// console.log(bondTitles);
+// console.log(bondTitles[Object.keys(bondTitles)]);
+
+// const bondTitles= console.log([bondFilms[i].title]);
+// bondFilms[bondTitles[i]] = 0
+
+
+// Create a new array oddBonds, of only the Bond films released on odd-numbered years.
+// const oddBonds = []
+// for(let i = 0; i < bondFilms.length; i++){
+// 	// if(bondFilms[i].year % ){
+// 		if ((bondFilms[i].year % 2) == 1){
+// 		oddBonds.push(bondFilms[i]);
+// }
+// }
+
+// console.log(oddBonds);
+
+
+
+for(let i =0; i < bondFilms.length; i++){
+	// .replace() += (bondFilms[i].gross);
+	// if(bondFilms[i].gross){
+
+	bondFilms[i].gross.replace(/$/g, "")
+
+	bondFilms[i].gross.replace(/,/g, "")
+
+		console.log(bondFilms[i].gross);
+			// let bondGross=console.log (bondFilms[i].gross)
+			
+	}
+// }
+// Determine the total cumulative gross of the Bond franchise, and console.log the result.
+// Hint: To make the grosses into usable numbers, look into the .replace Javascript method 
+// (there are many ways to do this, however). Look into parseInt() also.
+
